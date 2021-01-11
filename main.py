c7b5ba26cb0eee5d676401e868a7786fbd247f8b
Team 7 <rew222lp@gmail.com>
2021-01-11 12:48:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Komentārus var
+#rakstīt vairākās
+#rindās"""
+print("Sveika, pasaule!")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+print(f"Ja saskaitisim 5 ar 7, iegūsim {5+7}.")
+print("Sveika, "+"pasaule!")
+print("Andrejs Timčuks! "*5)
+print(365*24*60*60)
+print(0.1+0.2-0.3)
+
