c25e4c6cb23d40f0dda3b0e0e7595169cd1d2db5
Team 7 <rew222lp@gmail.com>
2021-01-11 13:21:42 +0000

    Datu ievadei

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ print("Andrejs Timčuks! "*5)
 print(365*24*60*60)
 print(0.1+0.2-0.3)
 print()
+
+#Mainigie
+a=5
+print(a)
+print(a+a)
+a=a+a  #Ar=piešķir vērtibu
+print(a)
+print(type(a))
+a=30.1
+print(type(a))
+
+#Nodokļi
+mani_ienakumi=367
+nodoklis=0.23 #10%
+mani_nodokļi=mani_ienakumi*nodoklis
+print(mani_nodokļi)
+
+#Datu ievade
+x=input("Ievadi vārdu: ")
+print("Tavs ievadītais vārds ir "+x)
+
